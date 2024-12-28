58a17e9d91184382a98fd810813e412239923c5f
Jagadeepgithub <jagadeep.kadam01@gmail.com>
2024-12-28 21:12:31 +0530

    commiting a python dog robot file

---
+++ robotdogbasic.py
@@ -0,0 +1,9 @@
+class Robot_Dog:
+    def __init__(self,name,breed):
+        self.name=name
+        self.breed=breed
+
+my_dog=Robot_Dog('Spot','Chihuaha')
+
+print(my_dog.name)
+print(my_dog.breed)
